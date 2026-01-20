cd3a6ff7aeb3fa87cd74c72ea46f2f68b3fd9b21
gray95 <gaurav.sinharay@swansea.ac.uk>
2026-01-20 21:40:02 +0000

    Test.cc refactored with the core code in its own function that is templated on the gauge group.

--- tests/IO/Test_ildg_reducedfmt_io.cc
+++ tests/IO/Test_ildg_reducedfmt_io.cc
@@ -34,21 +34,19 @@ using namespace std;
 using namespace Grid;
 
 //////////////////////////////////////////////
-//
-// this template function generates a
+// this template function returns a
 // LatticeGaugeField of the chosen gauge
 // group passed as a template argument.
-//
 //////////////////////////////////////////////
 template<class gaugeGroup>
-LatticeGaugeField generateHotFieldConfiguration( GridCartesian &Fine, std::vector<int> seed ) {
+LatticeGaugeField generateHotFieldConfiguration( GridCartesian &Grid, std::vector<int> seed = {83,8,34,59} ) {
 
-  GridParallelRNG   pRNGa(&Fine);
+  GridParallelRNG   pRNGa(&Grid);
   std::cout <<GridLogMessage<< " seeding... "<<std::endl;
   pRNGa.SeedFixedIntegers(seed);
   std::cout <<GridLogMessage<< " ...done "<<std::endl;
 
-  LatticeGaugeField Umu(&Fine);
+  LatticeGaugeField Umu(&Grid);
 
   if constexpr( std::is_same_v<gaugeGroup, GroupName::SU> == true ) {
     SU<Nc>::HotConfiguration(pRNGa,Umu);
@@ -57,6 +55,55 @@ LatticeGaugeField generateHotFieldConfiguration( GridCartesian &Fine, std::vecto
   } else { static_assert(true, "Grid does not recognise the gauge group"); }
 
   return Umu; 
+}
+
+///////////////////////////////////////////////////////////////
+// this template function generates writes a lattice
+// field of a given gaugeGroup to disk. It can write in
+// reduced format and single precision depending on 
+// the values of matrix_fmt and fp_fmt. 
+///////////////////////////////////////////////////////////////
+template<class gaugeGroup, int N, MatrixFormat matrix_fmt, FloatingPointFormat fp_fmt>
+void writeReadIldgConfiguration( LatticeGaugeField &Umu, GridCartesian &Grid, std::string file)  {
+
+  // if the IldgReader class is not able to handle
+  // the binary format do nothing and exit the function 
+  if constexpr( std::is_same_v<gaugeGroup,GroupName::Sp> && N%2==1) {
+    std::cout <<GridLogMessage<<"**************************************"<<std::endl;
+    std::cout <<GridLogMessage<< "CAN NOT WRITE REDUCED LATTICE" << std::endl;
+    std::cout <<GridLogMessage<< "For Sp fields Nc must be even and >= 4" << std::endl;
+    std::cout <<GridLogMessage<<"**************************************"<<std::endl;
+    return;
+  } 
+  if constexpr( std::is_same_v<gaugeGroup,GroupName::SU> && matrix_fmt==MatrixFormat::REDUCED && N>3)
+  { 
+    std::cout <<GridLogMessage<<"**************************************"<<std::endl;
+    std::cout <<GridLogMessage<< "CAN NOT WRITE REDUCED LATTICE" << std::endl;
+    std::cout << GridLogMessage << "currently when writing reduced SU fields Nc must be either 2 or 3" << std::endl;
+    std::cout <<GridLogMessage<<"**************************************"<<std::endl;
+    return;
+  }
+
+  FieldMetaData header;
+
+  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
+  std::cout <<GridLogMessage<<"*** Writing out ILDG cfg ***"<<std::endl;
+  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
+  IldgWriter _IldgWriter(Grid.IsBoss());
+  _IldgWriter.open(file);
+  _IldgWriter.writeConfiguration<gaugeGroup, matrix_fmt, fp_fmt>(Umu,4000,std::string("dummy_ildg_LFN"),std::string("dummy_config"));
+  _IldgWriter.close();
+
+  LatticeGaugeField Umu_saved = generateHotFieldConfiguration<gaugeGroup>(Grid);
+
+  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
+  std::cout <<GridLogMessage<<"** Reading back ILDG conf    *********"<<std::endl;
+  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
+  IldgReader _IldgReader;
+  _IldgReader.open(file);
+  _IldgReader.readConfiguration(Umu_saved, header);
+  _IldgReader.close();
+  std::cout <<GridLogMessage<< "norm2 Gauge Diff = "<<norm2((Umu_saved-Umu))<<std::endl;
 }
 
 int main (int argc, char ** argv)
@@ -68,85 +115,28 @@ int main (int argc, char ** argv)
 
   auto simd_layout = GridDefaultSimd(4,vComplex::Nsimd());
   auto mpi_layout  = GridDefaultMpi();
+
   Coordinate latt_size  ({8,8,8,16});
 
   std::vector<int> seed0 = {15,16,23,42};
-  std::vector<int> seed1 = {7,10,19,95};
-  std::vector<int> seed2 = {55,34,23,13};
+  std::vector<int> seed1 = {95,63,71,66};
 
-  GridCartesian  Fine(latt_size,simd_layout,mpi_layout);
+  GridCartesian  Grid(latt_size,simd_layout,mpi_layout);
 
-  // set the gauge group
-  using grpName = GroupName::SU;
+  LatticeGaugeField Umu   = generateHotFieldConfiguration<GroupName::SU>(Grid, seed0);
+  LatticeGaugeField UmuSp = generateHotFieldConfiguration<GroupName::Sp>(Grid, seed1);
 
-  LatticeGaugeField Umu       = generateHotFieldConfiguration<grpName>(Fine, seed0);
-  LatticeGaugeField Umu_diff  = generateHotFieldConfiguration<grpName>(Fine, seed1);
-  LatticeGaugeField Umu_saved = generateHotFieldConfiguration<grpName>(Fine, seed2);
-
-  // define enums for different field formats
-  FloatingPointFormat const fmt64 = FloatingPointFormat::IEEE64BIG; 
-  FloatingPointFormat const fmt32 = FloatingPointFormat::IEEE32BIG; 
-  MatrixFormat const noGrpRdc     = MatrixFormat::FULL;
-  MatrixFormat const GrpRdc       = MatrixFormat::REDUCED;
-
-  FieldMetaData header;
-
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  std::cout <<GridLogMessage<<"** Writing out FULL FAT ILDG cfg  ****"<<std::endl;
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  std::string file("./ckpoint_lat_test_FULLFAT");
-  IldgWriter _IldgWriter(Fine.IsBoss());
-  _IldgWriter.open(file);
-  _IldgWriter.writeConfiguration<grpName,noGrpRdc,fmt64>(Umu,4000,std::string("dummy_ildg_LFN"),std::string("dummy_config"));
-  _IldgWriter.close();
-  Umu_saved = Umu;
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  std::cout <<GridLogMessage<<"** Reading back ILDG conf    *********"<<std::endl;
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  IldgReader _IldgReader;
-  _IldgReader.open(file);
-  _IldgReader.readConfiguration(Umu,header);
-  _IldgReader.close();
-  Umu_diff = Umu - Umu_saved;
-  std::cout <<GridLogMessage<< "norm2 Gauge Diff = "<<norm2(Umu_diff)<<std::endl;
-
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  std::cout <<GridLogMessage<<"** Writing out reduced matrix ILDG cfg **"<<std::endl;
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  std::string file1("./ckpoint_lat_test_SEMISKIMMED");
-  IldgWriter _IldgWriter1(Fine.IsBoss());
-  _IldgWriter1.open(file1);
-  _IldgWriter1.writeConfiguration<grpName,GrpRdc,fmt64>(Umu,4000,std::string("dummy_ildg_LFN"),std::string("dummy_config"));
-  _IldgWriter1.close();
-  Umu_saved = Umu;
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  std::cout <<GridLogMessage<<"** Reading back ILDG conf    *********"<<std::endl;
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  IldgReader _IldgReader1;
-  _IldgReader1.open(file1);
-  _IldgReader1.readConfiguration(Umu,header);
-  _IldgReader1.close();
-  Umu_diff = Umu - Umu_saved;
-  std::cout <<GridLogMessage<< "norm2 Gauge Diff = "<<norm2(Umu_diff)<<std::endl;
-
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  std::cout <<GridLogMessage<<"*Writing out reduced single precision ILDG cfg*"<<std::endl;
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  std::string file2("./ckpoint_lat_test_SKIMMED");
-  IldgWriter _IldgWriter2(Fine.IsBoss());
-  _IldgWriter2.open(file2);
-  _IldgWriter2.writeConfiguration<grpName,GrpRdc,fmt32>(Umu,4000,std::string("dummy_ildg_LFN"),std::string("dummy_config"));
-  _IldgWriter2.close();
-  Umu_saved = Umu;
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  std::cout <<GridLogMessage<<"** Reading back ILDG conf    *********"<<std::endl;
-  std::cout <<GridLogMessage<<"**************************************"<<std::endl;
-  IldgReader _IldgReader2;
-  _IldgReader2.open(file2);
-  _IldgReader2.readConfiguration(Umu,header);
-  _IldgReader2.close();
-  Umu_diff = Umu - Umu_saved;
-  std::cout <<GridLogMessage<< "norm2 Gauge Diff = "<<norm2(Umu_diff)<<std::endl;
+  // write/read SU fields
+  writeReadIldgConfiguration<GroupName::SU, Nc, MatrixFormat::FULL, FloatingPointFormat::IEEE64BIG>(Umu, Grid, "./ckpoint_su_full64.4000");
+  writeReadIldgConfiguration<GroupName::SU, Nc, MatrixFormat::REDUCED, FloatingPointFormat::IEEE64BIG>(Umu, Grid, "./ckpoint_su_rdc64.4000");
+  writeReadIldgConfiguration<GroupName::SU, Nc, MatrixFormat::FULL, FloatingPointFormat::IEEE32BIG>(Umu, Grid, "./ckpoint_su_full32.4000");
+  writeReadIldgConfiguration<GroupName::SU, Nc, MatrixFormat::REDUCED, FloatingPointFormat::IEEE32BIG>(Umu, Grid, "./ckpoint_su_rdc32.4000");
+  
+  // write/read Sp fields 
+  writeReadIldgConfiguration<GroupName::Sp, Nc, MatrixFormat::FULL, FloatingPointFormat::IEEE64BIG>(UmuSp, Grid, "./ckpoint_sp_full64.4000");
+  writeReadIldgConfiguration<GroupName::Sp, Nc, MatrixFormat::REDUCED, FloatingPointFormat::IEEE64BIG>(UmuSp, Grid, "./ckpoint_sp_rdc64.4000");
+  writeReadIldgConfiguration<GroupName::Sp, Nc, MatrixFormat::FULL, FloatingPointFormat::IEEE32BIG>(UmuSp, Grid, "./ckpoint_sp_full32.4000");
+  writeReadIldgConfiguration<GroupName::Sp, Nc, MatrixFormat::REDUCED, FloatingPointFormat::IEEE32BIG>(UmuSp, Grid, "./ckpoint_sp_rdc32.4000");
 
   Grid_finalize();
 #endif
