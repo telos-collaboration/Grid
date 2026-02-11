3d72d3c6b199a4f22d521ac03c7afe5d91ce902e
gray95 <gaurav.sinharay@swansea.ac.uk>
2026-02-11 21:52:35 +0000

    adds unit test for reconstructSp and adds if-constexpr guards for SU/Sp differentiation

--- tests/IO/Test_io_mungers.cc
+++ tests/IO/Test_io_mungers.cc
@@ -34,9 +34,8 @@ using namespace Grid;
 
 // Unit tests to check the various (un)munger classes 
 // and functions in parallelIO/Metadata.h
-void check_reconstruct3();
+void check_reconstructSU();
 void check_reconstructSp();
-// void check reconstructSU();
 void checkBinarySimpleMungers();
 void checkGaugeSimpleMungers();
 void checkGaugeDoubleStoredMungers();
@@ -45,26 +44,26 @@ void checkGaugeSpmungers();
 
 // check result is still in SU, determinant==1
 // need to mock up an SU matrix 
-void check_reconstruct3() {
+void check_reconstructSU() {
 
   ColourMatrixD Ta, Tb, Tc, arg;
-  SU3::generator(1, Ta);
-  SU3::generator(4, Tb);
-  SU3::generator(6, Tc);
+  SU<Nc>::generator(1, Ta);
+  SU<Nc>::generator(4, Tb);
+  SU<Nc>::generator(6, Tc);
 
   const RealD  a(0.6), b(0.9), c(0.7);
 
   ColourMatrixD SU3_field;
   arg = timesI(a*Ta + b*Tb + c*Tc);
   SU3_field = Exponentiate(arg, 2.0); // what is RealD alpha??
-  std::cout << GridLogMessage << SU3_field()()(Nc-1,1) << std::endl;
+  //std::cout << GridLogMessage << SU3_field()()(Nc-1,1) << std::endl;
 
   //set last row equal to zero
   for(int j=0;j<Nc;j++) {
     SU3_field()()(Nc-1,j) = 0.0;
   }
 
-  std::cout << GridLogMessage << SU3_field()()(Nc-1,1) << std::endl;
+  //std::cout << GridLogMessage << SU3_field()()(Nc-1,1) << std::endl;
   LorentzColourMatrixD test = Zero();
 
   for(int mu=0; mu<Nd; mu++){
@@ -78,33 +77,25 @@ void check_reconstruct3() {
   // check result is unitary and det==1
   for(int mu=0;mu<Nd;mu++) {
     new_cm = peekIndex<LorentzIndex>(test, mu);
-    std::cout << GridLogMessage << new_cm()()(Nc-1,1) << std::endl;
+    //std::cout << GridLogMessage << new_cm()()(Nc-1,1) << std::endl;
     auto det = Determinant(new_cm);
-    std::cout << GridLogMessage << "det: " << det << " norm2: " << norm2(det) << std::endl;
-    std::cout << GridLogMessage << "norm2(UU^dag-1.0): " << norm2( new_cm*adj(new_cm)-1.0 ) << std::endl;
+    //std::cout << GridLogMessage << "det: " << det << " norm2: " << norm2(det) << std::endl;
+    //std::cout << GridLogMessage << "norm2(UU^dag-1.0): " << norm2( new_cm*adj(new_cm)-1.0 ) << std::endl;
     assert( abs(norm2(det)-1.0) < 1e-15 );
   }
 
-
 }
 
-// is result an Sp matrix, Zero(), One()?
-/*void check_reconstructSp() {
-
-  ColourMatrix Sp_field = Zero();
+void check_reconstructSp() {
 
   LorentzColourMatrix cm = Zero();
   LorentzColourMatrix cm_zero = Zero();
-  LorentzColourMatrix cm_one;
-  cm_one = 1.0;
 
   // should be zero matrix after reconstruction.
   reconstructSp(cm);
   assert(cm==cm_zero);
-
   
-  reconstructSp(cm);
-  
+  ColourMatrix Sp_field = Zero();
 
   const Complex  a(0.5, 0.5), abar(0.5, -0.5);
   const Complex  b(0.3, 0.9), bbar(0.3, -0.9);
@@ -121,24 +112,30 @@ void check_reconstruct3() {
       Sp_field()()(i,j) = b;
     }
   }
-  // fill bottom left
-  for(int i=2;i<Nc;i++) {
-    for(int j=0;j<Nc/2;j++) {
-      Sp_field()()(i,j) = -bbar;
-    }
-  }
-  // fill bottom right
-  for(int i=2;i<Nc;i++) {
-    for(int j=2;j<Nc;j++) {
-      Sp_field()()(i,j) = abar;
-    }
-  }
 
   LorentzColourMatrixD test = Zero();
 
+  for(int mu=0; mu<Nd; mu++){
+    pokeLorentz(test,Sp_field,mu);
+  }
+
+  reconstructSp(test);
+
+  // test Sp block structure 
+  //  A  B
+  // -B* A*
+  for(int mu=0; mu<Nd; mu++) {
+    auto Sp_cm = peekIndex<LorentzIndex>(test,mu);
+    for(int i=0;i<Nc/2;i++) {
+      for(int j=0;j<Nc/2;j++) {
+        assert( Sp_cm()()(i,j) == conjugate(Sp_cm()()(i+Nc/2,j+Nc/2)) );
+        assert( Sp_cm()()(i,j+Nc/2) == -conjugate(Sp_cm()()(i+Nc/2,j)) );
+      }
+    }
+  }
 
 }
-*/
+
 
 // mungeing between the same type should yield the same result.
 void checkBinarySimpleMungers() {
@@ -238,9 +235,9 @@ void checkGaugeDoubleStoredMungers() {
 // generate SU(3) matrix -> reduce it -> reconstruct it -> all good?
 void checkGauge3x2mungers() {
   ColourMatrixD Ta, Tb, Tc, arg;
-  SU3::generator(1, Ta);
-  SU3::generator(4, Tb);
-  SU3::generator(6, Tc);
+  SU<Nc>::generator(1, Ta);
+  SU<Nc>::generator(4, Tb);
+  SU<Nc>::generator(6, Tc);
 
   const RealD  a(0.6), b(0.9), c(0.7);
 
@@ -255,7 +252,7 @@ void checkGauge3x2mungers() {
     pokeLorentz(test,SU3_field,mu);
   }
 
-  std::cout << test << std::endl;
+  //std::cout << test << std::endl;
   
   // reduce field. Gauge3x2unmunger<out_type,in_type>
   Gauge3x2unmunger<LorentzColour2x3D,LorentzColourMatrixD> unmunger;
@@ -269,11 +266,10 @@ void checkGauge3x2mungers() {
   LorentzColourMatrixD test_recon;
   munger(test_rdc,test_recon);
 
-  std::cout << test_recon << std::endl;
+  //std::cout << test_recon << std::endl;
 
   // round-trip test
-  std::cout << norm2(test_recon-test);
-  //assert(norm2(test_recon-test)<1e-05);
+  assert(norm2(test_recon-test)<10e-7);
 
 }
 
@@ -316,21 +312,17 @@ void checkGaugeSpmungers() {
     pokeLorentz(test,Sp_field,mu);
   }
 
-  std::cout << test << std::endl;
+  //std::cout << test << std::endl;
   
   // reduce field. GaugeSpunmunger<out_type,in_type>
   GaugeSpunmunger<LorentzColourNx2ND,LorentzColourMatrixD> unmunger;
   LorentzColourNx2ND test_rdc = Zero();
   unmunger(test,test_rdc);
 
-  //std::cout << test_rdc << std::endl;
- 
   // reconstruct full field. GaugeSpmunger<in_type,out_type>
   GaugeSpmunger<LorentzColourNx2ND,LorentzColourMatrixD> munger;
   LorentzColourMatrixD test_recon;
   munger(test_rdc,test_recon);
-
-  std::cout << test_recon << std::endl;
 
   // round-trip test
   assert(test==test_recon);
@@ -348,13 +340,25 @@ int main (int argc, char ** argv)
   std::cout << GridLogMessage << "Nd is " << Nd << std::endl;
   std::cout << GridLogMessage << "Nc is " << Nc << std::endl;
   std::cout << GridLogMessage << "Nds is " << Nds << std::endl;
-  
-  check_reconstruct3();
-  //checkBinarySimpleMungers();
-  //checkGaugeSimpleMungers();
-  //checkGaugeDoubleStoredMungers();
-  //checkGauge3x2mungers();
-  //checkGaugeSpmungers();
+
+  if constexpr(Nc>1 && Nc<4) {  
+    std::cout << GridLogMessage << "Checking SU mungers " << std::endl;
+    check_reconstructSU();
+    checkGauge3x2mungers();
+  }
+
+  if constexpr(Nc>2 && Nc%2==0) {
+    std::cout << GridLogMessage << "Checking Sp mungers " << std::endl;
+    check_reconstructSp();
+    checkGaugeSpmungers();
+  }
+
+  std::cout << GridLogMessage << "Checking BinarySimple mungers " << std::endl;
+  checkBinarySimpleMungers();
+  std::cout << GridLogMessage << "Checking GaugeSimple mungers " << std::endl;
+  checkGaugeSimpleMungers();
+  std::cout << GridLogMessage << "Checking GaugeDoubleStored mungers " << std::endl;
+  checkGaugeDoubleStoredMungers();
 
   Grid_finalize();
 #endif
