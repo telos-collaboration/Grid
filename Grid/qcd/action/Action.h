b67dcb604f1481b3ed577eadfc621e2852e44bec
Ed Bennett <e.j.bennett@swansea.ac.uk>
2026-03-20 09:48:16 +0000

    add doxygen landing page for actions

--- Grid/qcd/action/Action.h
+++ Grid/qcd/action/Action.h
@@ -33,22 +33,57 @@ Author: paboyle <paboyle@ph.ed.ac.uk>
 #ifndef GRID_QCD_ACTION_H
 #define GRID_QCD_ACTION_H
 
+/*! \dir
+ * This directory holds implementations of lattice quantum fields that Grid can work with.
+ *
+ * To construct a particular action of interest,
+ * one should use instances of the relevant ``Action`` types for each term contributing to the action.
+ * For example,
+ * for 2+1 flavour QCD,
+ * one would need one gauge action instance (e.g. ``WilsonGaugeAction``),
+ * one a two-flavour pseudofermion action (e.g. ``TwoFlavourPseudoFermionAction``)
+ * for a given fermion action (e.g. ``MobiusFermionD``),
+ * and a one-flavour pseudofermion action (e.g. ``OneFlavourRationalPseudoFermionAction``)
+ * for the same fermion action.
+ *
+ * - The gauge/ directory contains actions for gauge fields
+ * - The fermion/ directory contains fermionic actions
+ * - The pseudofermion/ directory contains actions that wrap fermion fields for use in HMC;
+ *   for example,
+ *   as a single flavour (RHMC),
+ *   two flavours (HMC),
+ *   or a ratio (Hasenbusch acceleration, Pauli-Villars fields).
+ * - The scalar/ directory contains actions for scalar fields and interactions
+ * - The filter/ directory contains tools to manipulate
+ *   the distribution of conjugate momenta as part of the update/refresh
+ *
+ * All actions implement the interface defined in ActionBase.h.
+ */
+
 ////////////////////////////////////////////
 // Abstract base interface
 ////////////////////////////////////////////
 #include <Grid/qcd/action/ActionCore.h>
+/// \cond DO_NOT_DOCUMENT
 NAMESPACE_CHECK(ActionCore);
+/// \endcond
 ////////////////////////////////////////////////////////////////////////
 // Fermion actions; prevent coupling fermion.cc files to other headers
 ////////////////////////////////////////////////////////////////////////
 #include <Grid/qcd/action/fermion/FermionCore.h>
+/// \cond DO_NOT_DOCUMENT
 NAMESPACE_CHECK(FermionCore);
+/// \endcond
 #include <Grid/qcd/action/fermion/Fermion.h>
+/// \cond DO_NOT_DOCUMENT
 NAMESPACE_CHECK(Fermion);
+/// \endcond
 ////////////////////////////////////////
 // Pseudo fermion combinations for HMC
 ////////////////////////////////////////
 #include <Grid/qcd/action/pseudofermion/PseudoFermion.h>
+/// \cond DO_NOT_DOCUMENT
 NAMESPACE_CHECK(PseudoFermion);
+/// \endcond
 
 #endif
